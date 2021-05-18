a1d260203468eab1ac02683a4e0d428389787ef8
nelloy-git <nelloy93@yandex.ru>
2021-05-19 01:34:39 +0300

    Block added

--- src/Graphics/World/Block.ts
+++ src/Graphics/World/Block.ts
@@ -1,58 +1,77 @@
-import { Vec2 } from '../../Math';
-import { Object } from '../../Utils';
+import type { Mesh, Texture, VertexInformation } from 'love.graphics';
+
 import { Compositor } from '../Compositor';
-// import { Widget } from '../Widget';
 
-const HEIGHT = 50
-const WIDTH = 50
+const HEIGHT = 100
+const WIDTH = 100
 
-type Side = 'BOT' | 'REAR_LEFT' | 'READ_RIGHT' | 'FRONT_LEFT' | 'FRONT_RIGHT' | 'TOP'
+type Side = 'BOT' | 'LEFT_REAR' | 'RIGHT_REAR' | 'LEFT_FRONT' | 'RIGHT_FRONT' | 'TOP'
 
-function newCompositor(side: Side){
-    let comp = new Compositor(WIDTH, HEIGHT)
-    comp.transform = love.math.newTransform()
-
-    if (side == 'BOT' || side == 'TOP'){
-        comp.transform.translate(WIDTH / 2, 0)
-        comp.transform.scale()
-        comp.transform.rotate(math.pi / 2)
-        comp.transform.scale(1, 0.5)
-    } else if (side == 'FRONT_LEFT' || side == 'REAR_LEFT'){
-        comp.transform.shear(0, -0.5)
-    } else {
-        comp.transform.shear(0, 0.5)
-    }
-    
-    return comp
+const SIDE_MESHES: Record<Side, VertexInformation[]> = {
+    BOT: [[0, 0.75 * HEIGHT, 0, 0],
+          [WIDTH / 2, HEIGHT, 0, 1],
+          [WIDTH, 0.75 * HEIGHT, 1, 1],
+          [WIDTH / 2, 0.5 * HEIGHT, 1, 0]],
+    LEFT_REAR: [[0, 0.25 * HEIGHT, 0, 0],
+                [0, 0.75 * HEIGHT, 0, 1],
+                [WIDTH / 2, 0.5 * HEIGHT, 1, 1],
+                [WIDTH / 2, 0, 1, 0]],
+    RIGHT_REAR: [[WIDTH / 2, 0, 0, 0],
+                 [WIDTH / 2, 0.5 * HEIGHT, 0, 1],
+                 [WIDTH, 0.75 * HEIGHT, 1, 1],
+                 [WIDTH, 0.25 * HEIGHT, 1, 0]],
+    LEFT_FRONT: [[0, 0.25 * HEIGHT, 0, 0],
+                 [0, 0.75 * HEIGHT, 0, 1],
+                 [WIDTH / 2, HEIGHT, 1, 1],
+                 [WIDTH / 2, 0.5 * HEIGHT, 1, 0]],
+    RIGHT_FRONT: [[WIDTH / 2, 0.5 * HEIGHT, 0, 0],
+                  [WIDTH / 2, HEIGHT, 0, 1],
+                  [WIDTH, 0.75 * HEIGHT, 1, 1],
+                  [WIDTH, 0.25 * HEIGHT, 1, 0]],
+    TOP: [[0, 0.25 * HEIGHT, 0, 0],
+          [WIDTH / 2, 0.5 * HEIGHT, 0, 1],
+          [WIDTH, 0.25 * HEIGHT, 1, 1],
+          [WIDTH / 2, 0, 1, 0]],
 }
 
 export class Block extends Compositor {
 
     constructor(){
-        super()
-
-        this.draw_size = pixel_size.copy()
+        super(WIDTH, HEIGHT)
     }
 
-    draw(){
+    update(){
+        for (let side in this.__meshes){
+            if (this.__textures[<Side>side]){
+                this.drawDrawable(this.__meshes[<Side>side])
+            }
+        }
     }
 
-    drawBottom: (() => void) | undefined
-    drawBackLeft: (() => void) | undefined
-    drawBackRight: (() => void) | undefined
-    drawInside: (() => void) | undefined
-    drawFrontLeft: (() => void) | undefined
-    drawFrontRight: (() => void) | undefined
-    drawTop: (() => void) | undefined
+    getSide(side: Side){
+        return this.__textures[side]
+    }
 
-    draw_size: Vec2
+    setSide(side: Side, texture: Texture | undefined){
+        this.__textures[side] = texture
+        this.__meshes[side].setTexture(<Texture>texture)
+    }
 
-    private static __compositors: Record<Side, Compositor> = {
-        BOT: newCompositor('BOT'),
-        REAR_LEFT: newCompositor('REAR_LEFT'),
-        READ_RIGHT: newCompositor('READ_RIGHT'),
-        FRONT_LEFT: newCompositor('FRONT_LEFT'),
-        FRONT_RIGHT: newCompositor('FRONT_RIGHT'),
-        TOP: newCompositor('TOP'),
+    private __meshes: Record<Side, Mesh> = {
+        BOT: love.graphics.newMesh(SIDE_MESHES.BOT, 'fan'),
+        LEFT_REAR: love.graphics.newMesh(SIDE_MESHES.LEFT_REAR, 'fan'),
+        RIGHT_REAR: love.graphics.newMesh(SIDE_MESHES.RIGHT_REAR, 'fan'),
+        LEFT_FRONT: love.graphics.newMesh(SIDE_MESHES.LEFT_FRONT, 'fan'),
+        RIGHT_FRONT: love.graphics.newMesh(SIDE_MESHES.RIGHT_FRONT, 'fan'),
+        TOP: love.graphics.newMesh(SIDE_MESHES.TOP, 'fan'),
+    }
+
+    private __textures: Record<Side, Texture | undefined> = {
+        BOT: undefined,
+        LEFT_REAR: undefined,
+        RIGHT_REAR: undefined,
+        LEFT_FRONT: undefined,
+        RIGHT_FRONT: undefined,
+        TOP: undefined,
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,14 @@ import { Vec2 } from './Math'
 
 import { FileData, ImageData } from './Data'
 import { Image, Text, Window } from './Graphics'
-// import { Block } from './Graphics/World/Block'
-import { Canvas } from './Graphics/StdWidget/Canvas'
-import { Compositor } from './Graphics/Compositor'
-import { graphics } from 'love'
+import { Block } from './Graphics/World/Block'
 
 let fps: Text
-let test: Image[] = []
-// let world = new World(new Vec2(640, 480), 400)
 
-let comp = new Compositor(500, 500)
 let img_data = new ImageData(new FileData('test.jpg'))
-
 let img = love.graphics.newImage(img_data.data)
 
-// comp.canvas.renderTo(() => {
-//     graphics.draw(img, 0, 0, 0, 1, 1, 0, 0, 0, 0)
-// })
-
+let block = new Block()
 
 MainLoop.load.add(() => {
     Window.vsync = Window.VSync.DISABLED
@@ -31,14 +21,10 @@ MainLoop.load.add(() => {
     fps.draw_size = new Vec2(100, 100)
     fps.level = 10000
 
-    // print(comp.canvas.getWidth(), 'x', comp.canvas.getHeight())
-
-    comp.transform = love.math.newTransform()
-    comp.transform.translate(200, 0)
-    comp.transform.scale(1, 0.5)
-    comp.transform.rotate(math.pi / 4)
-
-    comp.drawDrawable(img)
+    block.setSide('TOP', img)
+    block.setSide('LEFT_FRONT', img)
+    block.setSide('RIGHT_FRONT', img)
+    block.update()
 })
 
 MainLoop.update.add((dt) => {
@@ -48,7 +34,8 @@ MainLoop.update.add((dt) => {
 })
 
 MainLoop.draw.add(() => {
-    graphics.draw(comp.canvas)
+    let [w, h] = Window.size.unpack()
+    love.graphics.draw(block.canvas, w / 2, h / 2, 0, 1, 1)
     // graphics.draw(img)
 })
 
